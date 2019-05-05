27abb34d90d547147020dc8fcf914c6423e9294c
wasaysir <mwasaysaeed@gmail.com>
2019-05-04 21:04:45 -0400

    Sms Listening and Sending working

--- app/src/main/java/com/example/littlebrother/MainActivity.java
+++ app/src/main/java/com/example/littlebrother/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.littlebrother;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import androidx.core.app.ActivityCompat;
+import static android.content.ContentValues.TAG;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.telephony.SmsManager;
+import android.provider.Telephony;
+import android.net.Uri;
+
+
+public class MainActivity extends Activity implements MessageListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Register sms listener
+        SmsListener.bindListener(this);
+        messageReceived("gamer");
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage("6505551212", null, "sms message", null, null);
+    }
+
+    @Override
+    public void messageReceived(String message) {
+        Toast.makeText(this, "New Message Received: " + message, Toast.LENGTH_SHORT).show();
     }
 }
+
